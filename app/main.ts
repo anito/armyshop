6178fe1ecc009c4f23e54b483273cd3d7a19ff90
Ich <info@anito.de>
2016-08-20 22:04:58 +0200

    initial commit

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,15 @@
-import {platformBrowserDynamic}    from '@angular/platform-browser-dynamic';
-import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
-import {AppModule} from './app.module';
+// The browser platform with a compiler
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-//platformBrowserDynamic().bootstrapModule(AppModule);
-bootstrap(AppComponent);
+// The app module
+import { AppModule } from './app.module';
+
+// Compile and launch the module
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
